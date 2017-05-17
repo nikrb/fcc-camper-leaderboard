2c4fa30ce436a5582e93ab458879a12409983320
nikrb <nik_rb@yahoo.com>
2017-05-17 21:01:58 +0100

    code data fetch

---
+++ src/FccActions.js
@@ -0,0 +1,28 @@
+
+export const getData = () => {
+  return fetch( "https://fcctop100.herokuapp.com/api/fccusers/top/recent", {
+    method: 'get',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then( checkStatus)
+  .then( parseJSON)
+  .then( function( response){ return response});
+};
+
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(`HTTP Error ${response.statusText}`);
+  error.status = response.statusText;
+  error.response = response;
+  console.log(error); // eslint-disable-line no-console
+  throw error;
+}
+
+function parseJSON(response) {
+  return response.json();
+}
