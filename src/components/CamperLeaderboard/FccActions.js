782509784808ec5ea3facdedb9af66ba4c45a80e
nikrb <nik_rb@yahoo.com>
2017-05-21 16:12:45 +0100

    rework fcc users ranking data fetch to simulate remote paging

--- src/components/CamperLeaderboard/FccActions.js
+++ src/components/CamperLeaderboard/FccActions.js
@@ -1,5 +1,14 @@
-
-export const getData = ( endpoint) => {
+/**
+ * FccActions.js
+ @function getData
+ @desc simulate getting remote paged data for fcc brownie points. We know how many
+ rows there are in total, so we fake that and the remote sorting.
+ @param endpoint {string}  - fcc user brownie point api, recent or alltime
+ @param start     - simulate paging start row
+ @param count     - simulate paging max row count
+ @param sort_direction - simulate sorting
+ */
+export const getData = ( endpoint, start, count, sort_column, sort_direction) => {
   return fetch( `https://fcctop100.herokuapp.com/api/fccusers/top/${endpoint}`, {
     method: 'get',
     headers: {
@@ -7,7 +16,29 @@ export const getData = ( endpoint) => {
       'Content-Type': 'application/json'
     }
   }).then( checkStatus)
-  .then( parseJSON);
+  .then( parseJSON)
+  .then( (response) => {
+    // fake add the id
+    const res = response.map( (item, ndx) => {
+      return {...item, id: ndx+1};
+    });
+    // fake sort the data
+    // we get the data sorted width direction 1 and zero isn't sorted
+    if( sort_direction === -1){
+      res.sort( ( a, b) => {
+        if( a[sort_column] < b[sort_column]) return -1;
+        if( a[sort_column] > b[sort_column]) return 1;
+        return 0;
+      });
+    }
+    return {
+      data: res.filter( ( row, ndx) => {
+        return ndx >= start && ndx < start+count;
+      }),
+      // we know how many rows there are
+      total_rows: 100
+    };
+  });
 };
 
 
