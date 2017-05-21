df19ae9c3e2894fab6aa2606a38cbcdfedba3227
nikrb <nik_rb@yahoo.com>
2017-05-22 00:48:16 +0100

    add PagedTable to Table exports

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,5 +3,6 @@ import TableHeader from './TableHeader';
 import SortableColumn from './SortableColumn';
 import TableStyles from './TableStyles';
 import Pager from './Pager';
+import PagedTable from './PagedTable';
 
-export {Table, TableHeader, SortableColumn, TableStyles, Pager};
+export {Table, TableHeader, SortableColumn, TableStyles, Pager, PagedTable};
