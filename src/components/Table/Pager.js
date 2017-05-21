e775ff8c260b934483d297e79a8cb42376e95c71
nikrb <nik_rb@yahoo.com>
2017-05-21 16:13:04 +0100

    add pager component

---
+++ src/components/Table/Pager.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Pager extends React.Component {
+  state = {
+    current_page_no: 0
+  };
+  static propTypes = {
+    total_rows: PropTypes.number,
+    display_count: PropTypes.number,
+    handlePageSelect: PropTypes.func
+  };
+  total_pages = 0;
+  handleClick = ( e) => {
+    let new_page_no = this.state.current_page_no;
+    switch( e.target.value){
+      case 'first_page':
+        new_page_no = 0;
+        break;
+      case 'prev_page':
+        new_page_no -= 1;
+        break;
+      case 'next_page':
+        new_page_no += 1;
+        break;
+      case 'last_page':
+        new_page_no = this.total_pages;
+        break;
+      default:
+        break;
+    }
+    // this.props.pageClicked( current_page_no);
+  };
+  render = () => {
+    const wrapper = {
+      display: "flex",
+      flexDirection: "row",
+      margin: "10px 0"
+    };
+    const btn = {
+      backgroundColor: "rgba( 64,64,192,0.5)",
+      borderRadius: "10px"
+    };
+    const { total_rows, display_count} = this.props;
+    this.total_pages = total_rows / display_count +1;
+
+    const numbered_page_buttons = Array.from({length: this.total_pages}, (v, i) => {
+      return (
+        <button key={i} type="button" style={btn} onClick={this.handleClick} value={i} >
+          {i+1}
+        </button>
+      );
+    });
+    const left_arrow = String.fromCharCode( 9664);
+    const right_arrow = String.fromCharCode( 9654);
+    return (
+      <div style={wrapper}>
+        <button type="button" style={btn} onClick={this.handleClick}  value='first_page' >{left_arrow+left_arrow}</button>
+        <button type="button" style={btn} onClick={this.handleClick}  value='prev_page' >{left_arrow}</button>
+        {numbered_page_buttons}
+        <button type="button" style={btn} onClick={this.handleClick}  value='next_page' >{right_arrow}</button>
+        <button type="button" style={btn} onClick={this.handleClick}  value='last_page' >{right_arrow}{right_arrow}</button>
+      </div>
+    );
+  };
+}
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,5 +2,6 @@ import Table from './Table';
 import TableHeader from './TableHeader';
 import SortableColumn from './SortableColumn';
 import TableStyles from './TableStyles';
+import Pager from './Pager';
 
-export {Table, TableHeader, SortableColumn, TableStyles};
+export {Table, TableHeader, SortableColumn, TableStyles, Pager};
