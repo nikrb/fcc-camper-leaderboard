93b58770c93c829b98f542d2d02e62f72a343e9c
nikrb <nik_rb@yahoo.com>
2017-05-22 01:07:51 +0100

    remove redundant PagedTable

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,6 +3,5 @@ import TableHeader from './TableHeader';
 import SortableColumn from './SortableColumn';
 import TableStyles from './TableStyles';
 import Pager from './Pager';
-import PagedTable from './PagedTable';
 
-export {Table, TableHeader, SortableColumn, TableStyles, Pager, PagedTable};
+export {Table, TableHeader, SortableColumn, TableStyles, Pager};
